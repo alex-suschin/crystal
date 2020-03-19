b4c69339b97e88ec54d06df8203e56a6e608dc8a
alex-suschin <alex_suschin@mail.ru>
2020-03-19 10:18:56 +0300

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
 import "./import/modules";
+
+$(document).ready(function() {
+    // new Vue({
+    //     el: '#app',
+    //     data: {
+    //         items: [
+    //             'Главная',
+    //             'Жителям',
+    //             'О компании',
+    //             'Аварийные службы',
+    //             'Контакты'
+    //         ],
+    //         show: false,
+    //     },
+    // })
+
+    $('.hamburger').click(function() {
+        $(this).toggleClass('hamburger_active');
+        $('body, html').addClass('ov_h');
+        if ($(this).hasClass('hamburger_active')) {
+            $('.top-menu').addClass('top_menu_active');
+        } else {
+            $('body, html').removeClass('ov_h');
+            $('.top-menu').removeClass('top_menu_active');
+        }
+    });
+});
